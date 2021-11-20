738ec376ad2db8857b0d294c9102232984d2abe4
Ilmira Museeva <museevai@gmail.com>
2021-11-20 12:09:32 -0800

    added cart modal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
 
 function App() {
 
@@ -17,6 +18,7 @@ function hideCartHandler(){
 
   return (
     <CartProvider>
+      <Cart />
 
     {cartIsShown && <div>Cart...</div>}
 
